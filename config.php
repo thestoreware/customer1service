2d9ae5f4ea15a29be9280b937bf4123b1c6aeb47
thestoreware <46706083+thestoreware@users.noreply.github.com>
2020-05-01 13:07:48 +0300

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn_string = "host=ec2-23-23-182-18.compute-1.amazonaws.com port=5432 dbname=dab1rre8bobmvc user=istjwlioywmcgf password=0a29057c718d800d2819983c848f79b30a31ff16ca9c7002266c40502c3af159 options='--client_encoding=UTF8'";
- 
-$mysqli = pg_connect($conn_string);
- 
-
