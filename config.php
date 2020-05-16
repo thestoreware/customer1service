958780a93039fb90c740055ddf8a8b5973743308
thestoreware <46706083+thestoreware@users.noreply.github.com>
2020-05-16 10:13:03 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,12 +2,9 @@
 
 //===================================================================================================================================================================================================================================
 		//connect to the cloudhost
-		$conn_string = "host=ec2-52-207-25-133.compute-1.amazonaws.com port=5432 dbname=d47jklh3b3t0ro user=zynwhhjixnngrq password=9a22c32f4e7f9004a82186d5a2fd0c89b93f30a6a98386f2eb0a1468eba1be10 options='--client_encoding=UTF8'";
+		$conn_string = "host=ec2-18-233-32-61.compute-1.amazonaws.com port=5432 dbname=de360f4shrlbjd user=dcbgrevvwddcbc password=de67d2731395f66851a3e0d5806e3ef3c21034096c46fb2993b1505428b75e9a options='--client_encoding=UTF8'";
 	//====================================================================================================================================================================================================================================
-		//connect to the localhost
-		//	$conn_string= "host=localhost port=5432 dbname=rddevoir user=postgres password=Sa@1234 options='--client_encoding=UTF8'";
-	//=========================================================================================================================
-		
+	
 		
 		$mysqli = pg_connect($conn_string);
  
